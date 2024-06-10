402359beb9f356ddda3dd7aabcebea2d9b845152
PersieDaCoder <thinhdpham2510@gmail.com>
2024-06-10 15:56:26 +0700

    build : implement db into code

--- src/Domain/Interfaces/Data/IRepositories/IUserRepository.cs
+++ src/Domain/Interfaces/Data/IRepositories/IUserRepository.cs
@@ -3,5 +3,5 @@ using Domain.Models;
 namespace Domain.Interfaces.Data.IRepositories;
 public interface IUserRepository : IRepositoryBase<User>
 {
-
+    Task<IEnumerable<User>> GetUserList(bool trackChanges = false, CancellationToken cancellationToken = default);
 }
