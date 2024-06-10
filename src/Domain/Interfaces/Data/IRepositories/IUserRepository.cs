123019fb6191245eba1d267892213f432ecbe809
PersieDaCoder <thinhdpham2510@gmail.com>
2024-06-10 15:58:22 +0700

    build : implement db into code

--- src/Domain/Interfaces/Data/IRepositories/IUserRepository.cs
+++ src/Domain/Interfaces/Data/IRepositories/IUserRepository.cs
@@ -3,5 +3,4 @@ using Domain.Models;
 namespace Domain.Interfaces.Data.IRepositories;
 public interface IUserRepository : IRepositoryBase<User>
 {
-    Task<IEnumerable<User>> GetUserList(bool trackChanges = false, CancellationToken cancellationToken = default);
 }
--- src/Infrastructure/Data/Repositories/UserRepository.cs
+++ src/Infrastructure/Data/Repositories/UserRepository.cs
@@ -5,11 +5,4 @@ using Microsoft.EntityFrameworkCore;
 namespace Infrastructure.Data.Repositories;
 public class UserRepository(WowToGoDBContext context) : RepositoryBase<User>(context), IUserRepository
 {
-    public async Task<IEnumerable<User>> GetUserList(bool trackChanges = false, CancellationToken cancellationToken = default)
-    {
-        IQueryable<User> query = _dbSet.Include(user => user.Orders);
-        return trackChanges
-            ? await query.ToListAsync(cancellationToken)
-            : await query.AsNoTracking().ToListAsync(cancellationToken);
-    }
 }
