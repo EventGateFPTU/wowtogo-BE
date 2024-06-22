40c2fdc434c269bfbbc8a1ee8d62e97c4e96104a
PersieDaCoder <thinhdpham2510@gmail.com>
2024-06-22 12:17:56 +0700

    feat: checkin ticket

--- src/Domain/Interfaces/Data/IRepositories/ITicketRepository.cs
+++ src/Domain/Interfaces/Data/IRepositories/ITicketRepository.cs
@@ -6,4 +6,5 @@ public interface ITicketRepository : IRepositoryBase<Ticket>
 {
     Task<GetTicketDetailsResponse?> GetTicketDetail(Guid ticketId, bool trackChanges = false, CancellationToken cancellationToken = default);
     Task<CreateTicketResponse?> GetCreatedTicketDetail(Guid ticketId, bool trackChanges = false, CancellationToken cancellationToken = default);
+    Task<Ticket?> GetTicketDetailByCode(string code, Guid showId, bool trackChanges = false, CancellationToken cancellationToken = default);
 }
