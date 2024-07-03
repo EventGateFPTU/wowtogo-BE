4a86c9be1541773928986a77e39b2953fed12212
PersieDaCoder <thinhdpham2510@gmail.com>
2024-07-03 16:22:23 +0700

    feat: upload background image url

--- src/API/Endpoints/EventEndpoints.cs
+++ src/API/Endpoints/EventEndpoints.cs
@@ -1,9 +1,6 @@
 using API.Endpoints.EndpointHandler.EventEndpointHandler.Commands;
 using API.Endpoints.EndpointHandler.EventEndpointHandler.Queries;
-using CloudinaryDotNet;
-using CloudinaryDotNet.Actions;
 using Swashbuckle.AspNetCore.Annotations;
-using System.Drawing;
 
 namespace API.Endpoints;
 public static class EventEndpoints
@@ -18,6 +15,7 @@ public static class EventEndpoints
         group.MapPost("/{eventId}/staff/{userId}", AddStaffEndpointHandler.Handle).WithMetadata(new SwaggerOperationAttribute("Add a staff"));
         group.MapPost("", CreateEventEndpointHandler.Handle).WithMetadata(new SwaggerOperationAttribute("Create an event"));
         // PUT
+        group.MapPut("/{eventId}", UploadBackgroundImageEndpointHandler.Handle).DisableAntiforgery().WithMetadata(new SwaggerOperationAttribute("Upload background image of an event"));
         // DELETE
         group.MapDelete("/staff/{staffId}", RemoveStaffEndpointHandler.Handle).WithMetadata(new SwaggerOperationAttribute("Delete a staff"));
         return group;
