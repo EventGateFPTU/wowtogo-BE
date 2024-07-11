95084406f0db160c5886c002fe6100d0875b9234
PersieDaCoder <thinhdpham2510@gmail.com>
2024-07-11 22:36:31 +0700

    feat: send email when order is confirm

--- src/UseCases/UC_Ticket/Commands/CreateTicket/CreateTicketHandler.cs
+++ src/UseCases/UC_Ticket/Commands/CreateTicket/CreateTicketHandler.cs
@@ -1,15 +1,21 @@
 using Ardalis.Result;
 using Domain.Interfaces.Data;
+using Domain.Interfaces.Email;
 using Domain.Models;
 using Domain.Responses.Responses_Ticket;
 using MediatR;
+using UseCases.Common.Models;
 using UseCases.Utils;
 
 namespace UseCases.UC_Ticket.Commands.CreateTicket;
-public class CreateTicketHandler(IUnitOfWork unitOfWork) : IRequestHandler<CreateTicketCommand, Result<CreateTicketResponse>>
+public class CreateTicketHandler(IUnitOfWork unitOfWork, IMailService mailService) : IRequestHandler<CreateTicketCommand, Result<CreateTicketResponse>>
 {
     public async Task<Result<CreateTicketResponse>> Handle(CreateTicketCommand request, CancellationToken cancellationToken)
     {
+        Attendee? checkingAttendee = await unitOfWork.AttendeeRepository.FindAsync(a => a.Id.Equals(request.AttendeeId), cancellationToken: cancellationToken);
+        if (checkingAttendee is null) return Result.NotFound("Attendee is not found !");
+        TicketType? checkingTicketType = await unitOfWork.TicketTypeRepository.FindAsync(tt => tt.Id.Equals(request.TicketTypeId), cancellationToken: cancellationToken);
+        if (checkingTicketType is null) return Result.NotFound("Ticket Type is not found !");
         Ticket ticket = new()
         {
             TicketTypeId = request.TicketTypeId,
@@ -20,6 +26,7 @@ public class CreateTicketHandler(IUnitOfWork unitOfWork) : IRequestHandler<Creat
         if (!await unitOfWork.SaveChangesAsync(cancellationToken)) return Result.Error("Failed to create ticket");
         CreateTicketResponse? ticketDetail = await unitOfWork.TicketRepository.GetCreatedTicketDetail(ticket.Id, cancellationToken: cancellationToken);
         if (ticketDetail == null) return Result.Error("Successfully created the ticket but failed to get its detail");
+        if (!await mailService.SendTicketGeneratedMailAsync(userName: checkingAttendee.Email, code: ticket.Code)) return Result.Error("Error sending mail");
         return Result.Success(ticketDetail);
     }
 }
