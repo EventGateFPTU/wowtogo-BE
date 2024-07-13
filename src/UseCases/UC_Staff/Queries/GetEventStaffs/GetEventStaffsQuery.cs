db96da2c3dd04b0b94f3388cc2a5665d986e211c
PersieDaCoder <thinhdpham2510@gmail.com>
2024-07-13 15:53:23 +0700

    refactor : apply paginated response to get staffs

--- src/UseCases/UC_Staff/Queries/GetEventStaffs/GetEventStaffsQuery.cs
+++ src/UseCases/UC_Staff/Queries/GetEventStaffs/GetEventStaffsQuery.cs
@@ -1,7 +1,8 @@
 ﻿using Ardalis.Result;
 using Domain.Responses.Responses_Staff;
+using Domain.Responses.Shared;
 using MediatR;
 
 namespace UseCases.UC_Staff.Queries.GetEventStaffs;
 
-public record GetEventStaffsQuery(Guid EventId, int PageNumber = 1, int PageSize = 10) : IRequest<Result<GetEventStaffsResponse>>;
+public record GetEventStaffsQuery(Guid EventId, int PageNumber = 1, int PageSize = 10) : IRequest<Result<PaginatedResponse<StaffResponse>>>;
