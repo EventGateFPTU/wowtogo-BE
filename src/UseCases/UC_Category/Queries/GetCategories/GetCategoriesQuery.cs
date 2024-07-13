fccea4899ee780c0121c29f3a6151fe4ee81db23
PersieDaCoder <thinhdpham2510@gmail.com>
2024-07-13 15:36:38 +0700

    refactor : apply paginated response to get categories

--- src/UseCases/UC_Category/Queries/GetCategories/GetCategoriesQuery.cs
+++ src/UseCases/UC_Category/Queries/GetCategories/GetCategoriesQuery.cs
@@ -1,6 +1,7 @@
 using Ardalis.Result;
 using Domain.Responses.Responses_Category;
+using Domain.Responses.Shared;
 using MediatR;
 
 namespace UseCases.UC_Category.Queries.GetCategories;
-public record GetCategoriesQuery(int PageNumber = 1, int PageSize = 10, string? SearchTerm = null) : IRequest<Result<GetCategoriesResponse>>;
+public record GetCategoriesQuery(int PageNumber = 1, int PageSize = 10, string? SearchTerm = null) : IRequest<Result<PaginatedResponse<CategoryDB>>>;
