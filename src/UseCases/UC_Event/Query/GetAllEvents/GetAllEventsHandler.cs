c42cec77f5fa55ada6ac21cc21503d9cbf7cef27
PersieDaCoder <thinhdpham2510@gmail.com>
2024-07-13 18:37:28 +0700

    refactor : apply paginated responses for get events

--- src/UseCases/UC_Event/Query/GetAllEvents/GetAllEventsHandler.cs
+++ src/UseCases/UC_Event/Query/GetAllEvents/GetAllEventsHandler.cs
@@ -1,23 +1,17 @@
 ﻿using Ardalis.Result;
 using Domain.Interfaces.Data;
 using Domain.Responses.Responses_Event;
+using Domain.Responses.Shared;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace UseCases.UC_Event.Query.GetAllEvents
 {
-    public class GetAllEventsHandler(IUnitOfWork unitOfWork) : IRequestHandler<GetAllEventsQuery, Result<GetAllEventsResponse>>
+    public class GetAllEventsHandler(IUnitOfWork unitOfWork) : IRequestHandler<GetAllEventsQuery, Result<PaginatedResponse<EventDB>>>
     {
-        public async Task<Result<GetAllEventsResponse>> Handle(GetAllEventsQuery request, CancellationToken cancellationToken)
+        public async Task<Result<PaginatedResponse<EventDB>>> Handle(GetAllEventsQuery request, CancellationToken cancellationToken)
         {
-            IEnumerable<EventDB> eventDBs = await unitOfWork.EventRepository.GetAllEventAsync(request.PageNumber, request.PageSize, request.SearchTerm, false, cancellationToken);
-            // if (!eventDBs.Any())  return Result.NotFound("Events are not found");
-            GetAllEventsResponse result = new(request.PageNumber, request.PageSize, eventDBs);
-            return Result.Success(result, "Events are found successfully");
+            PaginatedResponse<EventDB> eventDBs = await unitOfWork.EventRepository.GetAllEventAsync(request.PageNumber, request.PageSize, request.SearchTerm, false, cancellationToken);
+            return Result.Success(eventDBs, "Events are found successfully");
         }
     }
 }
