47a9ba57c88052d3be242c3ca0b99e56d4ce5cc4
kaitozu <43519768+kaitoz11@users.noreply.github.com>
2024-07-14 17:57:01 +0700

    feat: search user by email (#71)

--- src/API/Endpoints/UserEndpoints.cs
+++ src/API/Endpoints/UserEndpoints.cs
@@ -1,4 +1,6 @@
+using API.Endpoints.EndpointHandler.UserEndpointHandler.Queries;
 using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace API.Endpoints;
 public static class UserEndpoints
@@ -6,6 +8,9 @@ public static class UserEndpoints
     public static RouteGroupBuilder MapUserEndpoints(this RouteGroupBuilder group)
     {
         group.MapGet("", () => "hello users");
+        group.MapGet("search", GetUsersByEmailEndpointHandler.Handle)
+            .WithMetadata(new SwaggerOperationAttribute("Search user by email"))
+            .RequireAuthorization();
         return group;
     }
 }
---
+++ src/UseCases/UC_User/Queries/GetUsersByEmail/GetUsersByEmailQuery.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Result;
+using Domain.Responses.Responses_User;
+using Domain.Responses.Shared;
+using MediatR;
+
+namespace UseCases.UC_User.Queries.GetUsersByEmail;
+
+public record GetUsersByEmailQuery(
+    string SearchTerm,
+    int PageNumber = 1,
+    int PageSize = 7
+    ) : IRequest<Result<PaginatedResponse<PublicUserDetailResponse>>>;
