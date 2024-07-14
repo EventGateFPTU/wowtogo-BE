fe71b2fb7c6451f00d5998faf757911a9633ec11
k4it0z11 <luucuong26@gmail.com>
2024-07-14 17:55:19 +0700

    feat: search user by email

---
+++ src/API/Endpoints/EndpointHandler/UserEndpointHandler/Queries/GetUsersByEmailEndpointHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using UseCases.UC_User.Queries.GetUsersByEmail;
+
+namespace API.Endpoints.EndpointHandler.UserEndpointHandler.Queries;
+
+public class GetUsersByEmailEndpointHandler
+{
+    public static async Task<IResult> Handle(ISender sender, string searchTerm, int pageNumber = 1,
+        int pageSize = 7, CancellationToken cancellationToken = default)
+    {
+        var result = await sender.Send(new GetUsersByEmailQuery(
+            SearchTerm: searchTerm,
+            PageSize: pageSize,
+            PageNumber: pageNumber
+            ), cancellationToken);
+        if (result.IsSuccess) return Results.Ok(result);
+        return Results.NotFound(result);
+    }
+}
