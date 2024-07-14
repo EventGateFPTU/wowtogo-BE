36857a65ff0b60bae1a767f0c1b8521ba4a83f27
k4it0z11 <luucuong26@gmail.com>
2024-07-14 17:03:42 +0700

    should be final??

---
+++ src/Domain/Events/TicketTypes/TicketTypeCreatedEvent.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Events.Shared;
+
+namespace Domain.Events.TicketTypes;
+
+public class TicketTypeCreatedEvent(Guid eventId, Guid ticketTypeId) : BaseEvent
+{
+    public Guid EventId { get; set; } = eventId;
+    public Guid TicketTypeId { get; set; } = ticketTypeId;
+}
