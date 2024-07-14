ea8a4062bdccf382f878437541853c8ff1a75b63
kaitozu <43519768+kaitoz11@users.noreply.github.com>
2024-07-14 22:23:54 +0700

    done (#75)

--- src/UseCases/UC_Staff/Commands/Assign/AssignHandler.cs
+++ src/UseCases/UC_Staff/Commands/Assign/AssignHandler.cs
@@ -1,5 +1,7 @@
 ﻿using Ardalis.Result;
+using Domain.Events.Staffs;
 using Domain.Interfaces.Data;
+using Domain.Models;
 using MediatR;
 using UseCases.Common.Constants;
 using UseCases.Common.Contracts;
@@ -7,22 +9,42 @@ using UseCases.Common.Models;
 
 namespace UseCases.UC_Staff.Commands.Assign;
 
-public class AssignHandler(CurrentUser currentUser, IPermissionManager permissionManager): IRequestHandler<AssignCommand, Result>
+public class AssignHandler(CurrentUser currentUser, IPermissionManager permissionManager, IUnitOfWork unitOfWork): IRequestHandler<AssignCommand, Result>
 {
     public async Task<Result> Handle(AssignCommand request, CancellationToken cancellationToken)
     {
-        var userObj = RelationObjects.User(request.UserId.ToString());
-        var showObj = RelationObjects.Show(request.ShowId.ToString());
-        if (!await HasPermissionToAssignStaff(showObj, userObj)) 
+        if (!await HasPermissionToAssignStaff(request.ShowId, request.UserId)) 
             return Result.Forbidden();
-
-        var isSuccess = await permissionManager.PutPermission((userObj, Relations.ShowAssignee, showObj));
+        var staff = await unitOfWork.StaffRepository
+            .FindAsync(x => x.UserId.Equals(request.UserId), cancellationToken: cancellationToken);
+        if (staff is null) return Result.NotFound("Staff not found");
+        var newShowStaff = new ShowStaff
+        {
+            UpdatedAt = DateTimeOffset.UtcNow,
+            ShowId = request.ShowId,
+            StaffId = staff.Id
+        };
+        unitOfWork.ShowStaffRepository.Add(newShowStaff);
+        newShowStaff.AddDomainEvent(new StaffAssignEvent(
+            staffUserId: request.UserId,
+            showId: request.ShowId
+            ));
+        var isSuccess = await unitOfWork.SaveChangesAsync(cancellationToken);
         return isSuccess ? Result.SuccessWithMessage("Successfully assign staff permission") : Result.Error();
     }
 
-    private async Task<bool> HasPermissionToAssignStaff(string showObj, string userObj)
+    private async Task<bool> HasPermissionToAssignStaff(Guid showId, Guid staffUserId)
     {
+        var userObj = RelationObjects.User(staffUserId.ToString());
+        var showObj = RelationObjects.Show(showId.ToString());
         var currentUserObj = RelationObjects.User(currentUser.User!.Id.ToString());
+
+        var userIsAlreadyAssignee = await permissionManager.HasPermission(
+            userObj,
+            Relations.ShowAssignee,
+            showObj
+        );
+        
         var userIsStaffOfEvent = await permissionManager.HasPermission(
             userObj,
             Relations.CanBeAssignedShow,
@@ -35,6 +57,8 @@ public class AssignHandler(CurrentUser currentUser, IPermissionManager permissio
             showObj
             );
         
-        return userIsStaffOfEvent && hasPermission;
+        return !userIsAlreadyAssignee 
+               && userIsStaffOfEvent 
+               && hasPermission;
     }
 }
