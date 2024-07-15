1845e2ef6e9825d42b88d6df2db4338712e6260c
kaitozu <43519768+kaitoz11@users.noreply.github.com>
2024-07-15 16:45:22 +0700

    add to db when check in & add more validations (#78)
    * add to db when check in & add more validations
    
    * fix: checkin

--- src/UseCases/UC_Staff/Commands/Checkin/CheckinCommand.cs
+++ src/UseCases/UC_Staff/Commands/Checkin/CheckinCommand.cs
@@ -1,5 +1,6 @@
 using Ardalis.Result;
+using Domain.Responses.Responses_Attendee;
 using MediatR;
 
 namespace UseCases.UC_Staff.Commands.Checkin;
-public record CheckinCommand(string Code, Guid ShowId, string usedInFormat) : IRequest<Result>;
+public record CheckinCommand(string Code, Guid ShowId, string UsedInFormat) : IRequest<Result<AttendeeDetailResponse>>;
--- src/UseCases/UC_Staff/Commands/Checkin/CheckinHandler.cs
+++ src/UseCases/UC_Staff/Commands/Checkin/CheckinHandler.cs
@@ -2,19 +2,71 @@ using Ardalis.Result;
 using Domain.Enums;
 using Domain.Interfaces.Data;
 using Domain.Models;
+using Domain.Responses.Responses_Attendee;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
+using OpenFga.Sdk.ApiClient;
+using UseCases.Common.Constants;
+using UseCases.Common.Contracts;
+using UseCases.Common.Models;
+using UseCases.Mapper.Mapp_Attendee;
 
 namespace UseCases.UC_Staff.Commands.Checkin;
-public class CheckinHandler(IUnitOfWork unitOfWork) : IRequestHandler<CheckinCommand, Result>
+public class CheckinHandler(IUnitOfWork unitOfWork, CurrentUser currentUser, IPermissionManager permissionManager) : IRequestHandler<CheckinCommand, Result<AttendeeDetailResponse>>
 {
-    public async Task<Result> Handle(CheckinCommand request, CancellationToken cancellationToken)
+    public async Task<Result<AttendeeDetailResponse>> Handle(CheckinCommand request, CancellationToken cancellationToken)
     {
         Ticket? ticket = await unitOfWork.TicketRepository.GetTicketDetailByCode(request.Code, request.ShowId, trackChanges: true, cancellationToken: cancellationToken);
         if (ticket is null) return Result.NotFound("Ticket is not found");
+        
+        // TODO: When already checked-in, return attendee info
         if (ticket.IsCheckedIn()) return Result.Error("Ticket is already checked in");
-        if (!Enum.TryParse(request.usedInFormat, out UsedInFormatEnum usedInFormatEnum)) return Result.Error("Invalid used in format");
+         
+        // var checkedInHistory = await unitOfWork.CheckinRepository
+        //     .FindManyAsync(c => c.ShowId.Equals(request.ShowId) && c.TicketId.Equals(ticket.Id), cancellationToken: cancellationToken);
+        
+        if (!Enum.TryParse(request.UsedInFormat, out UsedInFormatEnum usedInFormatEnum))
+            return Result.Error("Invalid used in format");
+
+        if (!await CurrentUserCanCheckInTicket(ticket.Id))
+            return Result.Error("The current user can not check-in for this ticket");
+        
+        if (!await TicketCanPassShow(ticketId: ticket.Id, showId: request.ShowId))
+            return Result.Forbidden();
+        
         ticket.CheckIn(usedInFormatEnum);
+        var newCheckIn = new Domain.Models.Checkin
+        {
+            ShowId = request.ShowId,
+            TicketId = ticket.Id
+        };
+        unitOfWork.CheckinRepository.Add(newCheckIn);
+        
         if (!await unitOfWork.SaveChangesAsync(cancellationToken)) return Result.Error("Failed to check in ticket");
-        return Result.SuccessWithMessage("Ticket is checked in successfully");
+
+        var result = ticket.Attendee.MapToAttendeeDetailResponse(ticket);
+        return Result.Success(result, "Ticket is checked in successfully");
+    }
+
+    private async Task<bool> TicketCanPassShow(Guid ticketId, Guid showId)
+    {
+        var ticketObj = RelationObjects.Ticket(ticketId.ToString());
+        var showObj = RelationObjects.Show(showId.ToString());
+        return await permissionManager.HasPermission(
+            ticketObj,
+            Relations.CanCheckInShow,
+            showObj
+        );
+    }
+
+    private async Task<bool> CurrentUserCanCheckInTicket(Guid ticketId)
+    {
+        var currentUserObj = RelationObjects.User(currentUser.User!.Id.ToString());
+        var ticketObj = RelationObjects.Ticket(ticketId.ToString());
+        return await permissionManager.HasPermission(
+            currentUserObj,
+            Relations.CanCheckInTicket,
+            ticketObj
+        );
     }
 }
