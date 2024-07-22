721c37e9df759bfb305c2eb107775b3b3d8e5a09
Thai Trung Kien Nguyen <kiennttse171282@fpt.edu.vn>
2024-07-22 23:04:58 +0700

    authorize for update organizer

--- src/UseCases/UC_Organizer/Commands/UpdateOrganizer/UpdateOrganizerHandler.cs
+++ src/UseCases/UC_Organizer/Commands/UpdateOrganizer/UpdateOrganizerHandler.cs
@@ -1,20 +1,36 @@
 ﻿using Ardalis.Result;
 using Domain.Interfaces.Data;
+using Domain.Models;
 using MediatR;
 using UseCases.Common.Models;
 
 namespace UseCases.UC_Organizer.Commands.UpdateOrganizer
 {
-	public class UpdateOrganizerHandler(IUnitOfWork unitOfWork, CurrentUser currentUser) : IRequestHandler<UpdateOrganizerCommand, Result>
+	public class UpdateOrganizerHandler(IUnitOfWork unitOfWork, CurrentUser currentUser) : IRequestHandler<UpdateOrganizerCommand, Result<Organizer>>
 	{
-		public async Task<Result> Handle(UpdateOrganizerCommand request, CancellationToken cancellationToken)
+		public async Task<Result<Organizer>> Handle(UpdateOrganizerCommand request, CancellationToken cancellationToken)
 		{
-			var checkingOrganizer = await unitOfWork.OrganizerRepository.FindAsync(c => c.Id.Equals(request.Id), trackChanges: true, cancellationToken: cancellationToken);
+			
+			/*var checkingOrganizer = await unitOfWork.OrganizerRepository.FindAsync(c => c.Id.Equals(request.Id), trackChanges: true, cancellationToken: cancellationToken);
 			if (checkingOrganizer is null) return Result.NotFound("Organizer not found");
 			checkingOrganizer.OrganizationName = request.OrganizationName;
 			checkingOrganizer.Description = request.Description;
 			if (!await unitOfWork.SaveChangesAsync(cancellationToken)) return Result.Error("Failed to update organizer");
-			return Result.SuccessWithMessage("Organizer is updated successfully");
-		}
-	}
+			return Result.SuccessWithMessage("Organizer is updated successfully");*/
+
+            //check if the user is authorized to update the organizer
+			Organizer? organizer = await unitOfWork.OrganizerRepository.FindAsync(c => c.Id.Equals(request.Id), trackChanges: true, cancellationToken: cancellationToken);
+			
+			if(currentUser.User.Id != organizer.Id ) return Result.Unavailable("You are not authorized to update this organizer");
+            organizer.OrganizationName = request.OrganizationName;
+			organizer.Description = request.Description;
+
+			
+			if (!await unitOfWork.SaveChangesAsync(cancellationToken)) return Result.Error("Failed to update organizer");
+			return Result.Success(organizer,"Organizer is updated successfully");
+
+
+
+        }
+    }
 }
