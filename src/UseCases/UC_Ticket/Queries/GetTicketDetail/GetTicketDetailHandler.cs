4c6a5206cbcfae54b8de0de7f3b2acd00f6642c4
PersieDaCoder <thinhdpham2510@gmail.com>
2024-07-22 23:34:37 +0700

    fix : check if current user is owner of ticket

--- src/UseCases/UC_Ticket/Queries/GetTicketDetail/GetTicketDetailHandler.cs
+++ src/UseCases/UC_Ticket/Queries/GetTicketDetail/GetTicketDetailHandler.cs
@@ -3,12 +3,16 @@ using Domain.Interfaces.Data;
 using Domain.Models;
 using Domain.Responses.Responses_Ticket;
 using MediatR;
+using UseCases.Common.Models;
 
 namespace UseCases.UC_Ticket.Queries.GetTicketDetail;
-public record GetTicketDetailHandler(IUnitOfWork unitOfWork) : IRequestHandler<GetTicketDetailQuery, Result<GetTicketDetailsResponse>>
+public record GetTicketDetailHandler(IUnitOfWork unitOfWork, CurrentUser currentUser) : IRequestHandler<GetTicketDetailQuery, Result<GetTicketDetailsResponse>>
 {
     public async Task<Result<GetTicketDetailsResponse>> Handle(GetTicketDetailQuery request, CancellationToken cancellationToken)
     {
+        Ticket? checkingTicket = await unitOfWork.TicketRepository.GetTicketDetailById(id: request.TicketId, cancellationToken: cancellationToken);
+        if (checkingTicket is null) return Result.NotFound("Ticket is not found");
+        if (!checkingTicket.Attendee.UserId.Equals(currentUser.User!.Id)) return Result.Forbidden();
         GetTicketDetailsResponse? ticket = await unitOfWork.TicketRepository.GetTicketDetail(request.TicketId, cancellationToken: cancellationToken);
         if (ticket is null) return Result.NotFound("Ticket is not found");
         return Result.Success(ticket, "Ticket is found successfully");
