51294e5a4dca6780f9b366e110919c3e48bbdd80
PersieDaCoder <thinhdpham2510@gmail.com>
2024-07-23 18:02:59 +0700

    fix : check if current user is organizer or staff

--- src/UseCases/UC_Order/Queries/GetOrdersByEvent/GetOrdersByEventHandler.cs
+++ src/UseCases/UC_Order/Queries/GetOrdersByEvent/GetOrdersByEventHandler.cs
@@ -1,15 +1,22 @@
 using Ardalis.Result;
 using Domain.Interfaces.Data;
+using Domain.Models;
 using Domain.Responses.Responses_Order;
 using Domain.Responses.Shared;
 using MediatR;
+using UseCases.Common.Models;
 
 namespace UseCases.UC_Order.Queries.GetOrdersByEvent;
-public class GetOrdersByEventHandler(IUnitOfWork unitOfWork) : IRequestHandler<GetOrdersByEventQuery, Result<PaginatedResponse<OrderResponse>>>
+public class GetOrdersByEventHandler(IUnitOfWork unitOfWork, CurrentUser currentUser) : IRequestHandler<GetOrdersByEventQuery, Result<PaginatedResponse<OrderResponse>>>
 {
     public async Task<Result<PaginatedResponse<OrderResponse>>> Handle(GetOrdersByEventQuery request, CancellationToken cancellationToken)
     {
+        Event? checkingEvent = await unitOfWork.EventRepository.GetEventWithOrganizer(request.Id, cancellationToken: cancellationToken);
+        if (checkingEvent is null) return Result.NotFound("Event is not found");
+        if (!IsCurrentUserOrganizerOrStaff(checkingEvent)) return Result.Forbidden();
         PaginatedResponse<OrderResponse> result = await unitOfWork.OrderRepository.GetOrdersByEventAsync(request.Id, request.PageNumber, request.PageSize, cancellationToken: cancellationToken);
         return Result.Success(result, "Get Order Successfully");
     }
+    private bool IsCurrentUserOrganizerOrStaff(Event @event)
+        => @event.Organizer.Id.Equals(currentUser.User!.Id) || @event.Staffs.Any(s => s.UserId.Equals(currentUser.User!.Id));
 }
