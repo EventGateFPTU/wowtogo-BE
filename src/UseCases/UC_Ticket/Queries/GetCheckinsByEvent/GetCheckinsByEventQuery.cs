3f7a19ac2494d8bccf20a25e47630f9702039920
PersieDaCoder <thinhdpham2510@gmail.com>
2024-07-23 14:45:31 +0700

    fix: check if current user is staff or organizer

--- src/UseCases/UC_Ticket/Queries/GetCheckinsByEvent/GetCheckinsByEventQuery.cs
+++ src/UseCases/UC_Ticket/Queries/GetCheckinsByEvent/GetCheckinsByEventQuery.cs
@@ -1,10 +1,9 @@
 using Ardalis.Result;
 using Domain.Responses.Responses_Checkin;
-using Domain.Responses.Responses_Ticket;
 using Domain.Responses.Shared;
 using MediatR;
 
-namespace UseCases.UC_Ticket.Query.GetCheckinsByEvent;
+namespace UseCases.UC_Ticket.Queries.GetCheckinsByEvent;
 public record GetCheckinsByEventQuery(
     Guid EventId,
     int PageNumber,
