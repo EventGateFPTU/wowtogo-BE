aedb9014b34f6860395b21d628b0efc1781057ca
ezarp <quangchieens1@gmail.com>
2024-07-24 23:35:26 +0700

    update: checkin flow

--- src/UseCases/UC_Staff/Commands/Checkin/CheckinCommand.cs
+++ src/UseCases/UC_Staff/Commands/Checkin/CheckinCommand.cs
@@ -3,4 +3,4 @@ using Domain.Responses.Responses_Attendee;
 using MediatR;
 
 namespace UseCases.UC_Staff.Commands.Checkin;
-public record CheckinCommand(string Code, Guid ShowId, string UsedInFormat) : IRequest<Result<AttendeeDetailResponse>>;
+public record CheckinCommand(Guid TicketId, Guid ShowId, string UsedInFormat) : IRequest<Result<AttendeeDetailResponse>>;
--- src/UseCases/UC_Staff/Commands/Checkin/CheckinHandler.cs
+++ src/UseCases/UC_Staff/Commands/Checkin/CheckinHandler.cs
@@ -4,26 +4,22 @@ using Domain.Interfaces.Data;
 using Domain.Models;
 using Domain.Responses.Responses_Attendee;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
-using OpenFga.Sdk.ApiClient;
 using UseCases.Common.Constants;
 using UseCases.Common.Contracts;
 using UseCases.Common.Models;
-using UseCases.Mapper.Mapp_Attendee;
 
 namespace UseCases.UC_Staff.Commands.Checkin;
 public class CheckinHandler(IUnitOfWork unitOfWork, CurrentUser currentUser, IPermissionManager permissionManager) : IRequestHandler<CheckinCommand, Result<AttendeeDetailResponse>>
 {
     public async Task<Result<AttendeeDetailResponse>> Handle(CheckinCommand request, CancellationToken cancellationToken)
     {
-        Ticket? ticket = await unitOfWork.TicketRepository.GetTicketDetailByCode(request.Code, request.ShowId, trackChanges: true, cancellationToken: cancellationToken);
+        var ticket = await unitOfWork.TicketRepository.FindAsync(x => x.Id == request.TicketId, trackChanges: true, cancellationToken: cancellationToken);
         if (ticket is null) return Result.NotFound("Ticket is not found");
 
         // TODO: When already checked-in, return attendee info
-        // if (ticket.IsCheckedIn()) return Result.Error("Ticket is already checked in");
-
-        // var checkedInHistory = await unitOfWork.CheckinRepository
-        //     .FindManyAsync(c => c.ShowId.Equals(request.ShowId) && c.TicketId.Equals(ticket.Id), cancellationToken: cancellationToken);
+        var checkin = await unitOfWork.CheckinRepository
+            .FindAsync(x => x.TicketId == ticket.Id && x.ShowId == request.ShowId, cancellationToken: cancellationToken);
+        if (checkin is not null) return Result.Error("Ticket is already checked in");
 
         if (!Enum.TryParse(request.UsedInFormat, out UsedInFormatEnum usedInFormatEnum))
             return Result.Error("Invalid used in format");
@@ -44,8 +40,7 @@ public class CheckinHandler(IUnitOfWork unitOfWork, CurrentUser currentUser, IPe
 
         if (!await unitOfWork.SaveChangesAsync(cancellationToken)) return Result.Error("Failed to check in ticket");
 
-        var result = ticket.Attendee.MapToAttendeeDetailResponse(ticket);
-        return Result.Success(result, "Ticket is checked in successfully");
+        return Result.Success();
     }
 
     private async Task<bool> TicketCanPassShow(Guid ticketId, Guid showId)
