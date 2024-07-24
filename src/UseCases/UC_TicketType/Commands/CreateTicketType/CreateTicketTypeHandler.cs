2baf969b141e2e22ae0a5ac666224a2a0980f396
PersieDaCoder <100859829+PersieDaCoder@users.noreply.github.com>
2024-07-24 14:22:32 +0700

    fix : check if current user owns event (#140)

--- src/UseCases/UC_TicketType/Commands/CreateTicketType/CreateTicketTypeHandler.cs
+++ src/UseCases/UC_TicketType/Commands/CreateTicketType/CreateTicketTypeHandler.cs
@@ -4,13 +4,15 @@ using Domain.Interfaces.Data;
 using Domain.Models;
 using Domain.Responses.Responses_TicketType;
 using MediatR;
+using UseCases.Common.Models;
 using UseCases.Mapper.Mapper_TicketType;
 
 namespace UseCases.UC_TicketType.Commands.CreateTicketType;
-public class CreateTicketTypeHandler(IUnitOfWork unitOfWork) : IRequestHandler<CreateTicketTypeCommand, Result<CreateTicketTypeResponse>>
+public class CreateTicketTypeHandler(IUnitOfWork unitOfWork, CurrentUser currentUser) : IRequestHandler<CreateTicketTypeCommand, Result<CreateTicketTypeResponse>>
 {
     public async Task<Result<CreateTicketTypeResponse>> Handle(CreateTicketTypeCommand request, CancellationToken cancellationToken)
     {
+        // check validation for the ticket type
         if (request.FromDate > request.ToDate) return Result.Error("From date should be less than to date");
         if (request.Amount < 0) return Result.Error("Amount should be greater than 0");
         if (request.LeastAmountBuy < 0) return Result.Error("Least amount buy should be greater than 0");
@@ -19,6 +21,10 @@ public class CreateTicketTypeHandler(IUnitOfWork unitOfWork) : IRequestHandler<C
         if (request.Price < 0) return Result.Error("Price should be greater than 0");
         if (request.Amount < request.LeastAmountBuy) return Result.Error("Amount should be greater than least amount buy");
         if (request.Amount < request.MostAmountBuy) return Result.Error("Amount should be greater than most amount buy");
+        Event? checkingEvent = await unitOfWork.EventRepository.GetEventWithOrganizer(request.EventId, cancellationToken: cancellationToken);
+        if (checkingEvent is null) return Result.NotFound("Event is not found");
+        if (!IsCurrentUserOwnEvent(checkingEvent)) return Result.Forbidden();
+        // new ticket type
         Guid ticketTypeId = Guid.NewGuid();
         TicketType ticketType = new()
         {
@@ -44,4 +50,6 @@ public class CreateTicketTypeHandler(IUnitOfWork unitOfWork) : IRequestHandler<C
         if (!await unitOfWork.SaveChangesAsync(cancellationToken)) return Result.Error("Failed to create ticket type");
         return Result.Success(ticketType.MapToTicketTypeResponse(), "Ticket type is created successfully");
     }
+    private bool IsCurrentUserOwnEvent(Event checkingEvent)
+        => checkingEvent.Organizer.Id.Equals(currentUser.User!.Id);
 }
