47fd570ead9010f5c9d22468c1814e1ce57b0755
Chien Nguyen Quang <quangchieens1@gmail.com>
2024-10-08 22:40:50 +0700

    asdf

--- src/UseCases/UC_Event/Query/SearchEvents/SearchEventsHandler.cs
+++ src/UseCases/UC_Event/Query/SearchEvents/SearchEventsHandler.cs
@@ -6,11 +6,11 @@ using MediatR;
 
 namespace UseCases.UC_Event.Query.SearchEvents;
 
-public class SearchEventsHandler(IUnitOfWork unitOfWork) : IRequestHandler<SearchEventsQuery, PaginatedResponse<EventDB>>
+public class SearchEventsHandler(IUnitOfWork unitOfWork) : IRequestHandler<SearchEventsQuery, PaginatedResponse<GetEventResponse>>
 {
-    public async Task<PaginatedResponse<EventDB>> Handle(SearchEventsQuery request, CancellationToken cancellationToken)
+    public async Task<PaginatedResponse<GetEventResponse>> Handle(SearchEventsQuery request, CancellationToken cancellationToken)
     {
-        PaginatedResponse<EventDB> eventDBs = await unitOfWork.EventRepository.SearchEventsAsync(request.CategoryIds,
+        PaginatedResponse<GetEventResponse> eventDBs = await unitOfWork.EventRepository.SearchEventsAsync(request.CategoryIds,
             request.PageNumber, request.PageSize, request.SearchTerm, request.Location, request.Date, false,
             cancellationToken);
         return Result.Success(eventDBs, "Events are found successfully");
